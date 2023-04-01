0c17ebff32156378a53ac8241eeca47ebfabe4f2
Maks Semenko <max_semenko@ukr.net>
2023-04-01 18:45:17 +0300

    add Generics RetrieveAPIView

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,8 @@
-from django.forms.models import model_to_dict
+# from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from products.models import Product
+# from products.models import Product
 from products.serializers import ProductSerializer
 
 @api_view(["POST"])
