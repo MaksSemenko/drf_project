3101a4643ff07ffda3ac72ff7b4ed29459eef8c6
Maks Semenko <max_semenko@ukr.net>
2023-04-02 20:24:19 +0300

    add DestroyAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -38,6 +38,14 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
         if not instance.content:
             instance.content = instance.title
 
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+
 @api_view(["GET", "POST"])
 def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method
