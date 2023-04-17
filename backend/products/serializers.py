00bc9ba9f2885b90d2f011d237b0f257ffc452d6
Maks Semenko <max_semenko@ukr.net>
2023-04-17 21:05:01 +0300

    add custom validation with serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 from .models import Product
-
+from .validators import validate_title_no_hello, unique_product_title
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
@@ -11,6 +11,9 @@ class ProductSerializer(serializers.ModelSerializer):
         lookup_field='pk',
     )
     email = serializers.EmailField(write_only=True)
+    title = serializers.CharField(validators=[validate_title_no_hello,
+                                              unique_product_title])
+
     class Meta:
         model = Product
         fields = [
@@ -24,6 +27,14 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+
+    # def validate_title(self, value):
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__exact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
 
     def create(self, validated_data):
         email = validated_data.pop('email')
